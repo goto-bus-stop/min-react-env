1adee96d4900d2fb12509105a73533b790733ff7
Renée Kooi <renee@kooi.me>
2020-03-25 14:11:14 +0100

    make it work with React 15

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-var document = require('min-document')
+var BaseElement = require('min-document/dom-element')
+var BaseDocument = require('min-document/document')
+
+function Element (tagName, document) {
+  BaseElement.call(this, tagName, document)
+}
+Element.prototype = Object.create(BaseElement.prototype)
+
+Object.defineProperties(Element.prototype, {
+  firstChild: {
+    get: function () { return this.childNodes[0] }
+  },
+  lastChild: {
+    get: function () { return this.childNodes[this.childNodes.length - 1] }
+  }
+})
+
+function Document () {
+  BaseDocument.call(this)
+}
+Document.prototype = Object.create(BaseDocument.prototype)
+
+Document.prototype.createElement = function (tagName) {
+  return new Element(tagName, this)
+}
 
 var window = {
   // React does an instanceof check on this
   HTMLIFrameElement: function () {},
-  document: document
+  document: new Document()
 }
 
 module.exports = {
   window: window,
-  document: document
+  document: window.document
 }
--- test/index.js
+++ test/index.js
@@ -9,9 +9,14 @@ test('it renders', (t) => {
   t.plan(3)
   var wrapper = document.createElement('div')
 
+  var expected = '<h1>It\'s tiny!</h1>'
+  if (/^15\./.test(React.version)) {
+    expected = '<h1 data-reactroot="">It\'s tiny!</h1>'
+  }
+
   ReactDOM.render(React.createElement('h1', {}, 'It\'s tiny!'), wrapper, () => {
     t.equal(wrapper.childNodes.length, 1)
     t.equal(wrapper.childNodes[0].tagName, 'H1')
-    t.equal(wrapper + '', '<div><h1>It\'s tiny!</h1></div>')
+    t.equal(wrapper + '', '<div>' + expected + '</div>')
   })
 })
